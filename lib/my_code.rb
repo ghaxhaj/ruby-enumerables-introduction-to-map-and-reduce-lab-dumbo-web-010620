94663cb81757eb6d0230d491c5bf0f76445db003
wise-integer-6834 <ghaxhaj@fordham.edu>
2020-01-06 04:00:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,6 +2,6 @@ def map_to_negativize(source_array)
   negative = []
   i = 0
   while i <source_array.length do
-    negative = (source_array * -1)
+    negative = source_array * -1
     i += 1
   end
