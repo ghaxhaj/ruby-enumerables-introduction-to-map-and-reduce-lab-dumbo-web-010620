2face14e4a000e3241dcc5cec393bd60cecd4208
wise-integer-6834 <ghaxhaj@fordham.edu>
2020-01-06 04:11:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -47,3 +47,15 @@ def reduce_to_total(source_array, starting_point=0)
   end
   return new
 end
+
+def reduce_to_all_true(source_array)
+  i = 0
+  total = []
+  while i < source_array.length do
+    if source_array[i] == true
+      total << source_array[i]
+    else
+      nil
+    end
+    return total
+  end 
